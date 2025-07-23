8260ec324620ca58a3bfcc69ff5d8c4515267753
Emmanuel Onu <sireconu@gmail.com>
2025-07-23 18:40:26 +0100

    Added FAQs section

--- src/script.js
+++ src/script.js
@@ -2,9 +2,8 @@ let todaysDates = new Date();
 let x = todaysDates.toDateString();
 document.getElementById("date").textContent = x;
 
-
 const textOutput = document.querySelector(".text-output");
-let text  = textOutput.innerHTML
+let text = textOutput.innerHTML;
 
 let count = 0;
 
@@ -30,65 +29,63 @@ document.addEventListener("DOMContentLoaded", function () {
   }).mount();
 });
 
-
+const path = document.querySelector("path");
 let darkMode = document.getElementById("darkMode");
 darkMode.addEventListener("click", () => {
   if (darkMode.classList.contains("black")) {
     darkMode.setAttribute("fill", "white");
     darkMode.classList.remove("black");
+    // path.fill = "white";
 
     document
       .querySelector("body")
       .classList.remove("bg-[#222]", "text-gray-200");
   } else {
     darkMode.setAttribute("fill", "black");
-
+    // path.fill = "black";
     darkMode.classList.add("black");
     document.querySelector("body").classList.add("bg-[#222]", "text-gray-200");
   }
 });
 
-
 const pTags = document.getElementsByTagName([..."p"]);
 const h2s = document.querySelectorAll("h2");
 
-
 const range = document.querySelector("#range-val");
 
-
 range.addEventListener("input", () => {
-  
   for (const h2 of h2s) {
-   h2.style.fontSize=`${range.value}rem`
-     
+    h2.style.fontSize = `${range.value}rem`;
   }
   for (const pTag of pTags) {
     pTag.style.fontSize = `${range.value}rem`;
   }
-  
+
   // console.log(`${range.value}rem`);
 });
 
 const thursdayAdoration = document.querySelector("#thursday-adoration");
-const main = document.querySelector("#main");
+const mainPages = document.querySelectorAll(".landing-page");
 const thurdayPrayerButton = document.querySelector(".thurday-prayer-button");
 
-thurdayPrayerButton.addEventListener('click', () => {
-  thursdayAdoration.classList.toggle('hidden')
-  main.classList.toggle('hidden')
-  scrollTo({ top: 0, behavior: "smooth" });
-})
-document.getElementById('exit').onclick  = function toggleMain() {
-  thursdayAdoration.classList.toggle("hidden");
-  main.classList.toggle("hidden");
-  scrollTo({ top: 0, behavior: "smooth" });
-}
 
-document.getElementById('finish').onclick  = function toggleMain() {
-  thursdayAdoration.classList.toggle("hidden");
-  main.classList.toggle("hidden");
-  scrollTo({ top: 0, behavior: "smooth" });
-}
+
+  thurdayPrayerButton.addEventListener("click", () => {
+    thursdayAdoration.classList.toggle("hidden");
+    for (const mainPage of mainPages) mainPage.classList.toggle("hidden");
+    scrollTo({ top: 0, behavior: "smooth" });
+  });
+  document.getElementById("exit").onclick = function toggleMain() {
+    thursdayAdoration.classList.toggle("hidden");
+    for (const mainPage of mainPages) mainPage.classList.toggle("hidden");
+    scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  document.getElementById("finish").onclick = function toggleMain() {
+    thursdayAdoration.classList.toggle("hidden");
+    for (const mainPage of mainPages) mainPage.classList.toggle("hidden");
+    scrollTo({ top: 0, behavior: "smooth" });
+  };
 
 
 const openNav = document.querySelector(".open-nav");
@@ -101,5 +98,3 @@ closeSetting.addEventListener("click", () => {
 openNav.addEventListener("click", () => {
   openSetting.classList.toggle("h-[338px]");
 });
-
-
